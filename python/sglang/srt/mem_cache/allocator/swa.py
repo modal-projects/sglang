3d006d04dc10fcdb0c580da46edfa35ad09690af
Leon Gao <leon.gao19@gmail.com>
2026-08-08 03:34:44 -0700

    [cherry-pick from upstream] Fix batched KV free aliasing

--- python/sglang/srt/mem_cache/allocator/swa.py
+++ python/sglang/srt/mem_cache/allocator/swa.py
@@ -324,7 +324,7 @@ class SWATokenToKVPoolAllocator(BaseTokenToKVPoolAllocator):
             self.full_attn_allocator.free(free_index)
             self.free_swa(free_index)
         else:
-            self.free_group.append(free_index)
+            self.free_group.append(self._copy_for_free_group(free_index))
         assert (
             self.full_attn_allocator.available_size() <= self.full_attn_allocator.size
         )
@@ -483,7 +483,7 @@ class PureSWATokenToKVPoolAllocator(SWATokenToKVPoolAllocator):
         if self.is_not_in_free_group:
             self.swa_attn_allocator.free(free_index[free_index > 0])
         else:
-            self.free_group.append(free_index)
+            self.free_group.append(self._copy_for_free_group(free_index))
         assert self.swa_attn_allocator.available_size() <= self.swa_attn_allocator.size
 
     def free_swa(self, free_index: torch.Tensor):
